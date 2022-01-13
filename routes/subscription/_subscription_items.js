200a0f4091c7e4f0a3eac7ad6af2eb63fecee752
fsmutimeer <ishpata@hotmail.com>
2022-01-12 23:31:00 -0800

    subscription items usage add

--- routes/subscription/_subscription_items.js
+++ routes/subscription/_subscription_items.js
@@ -67,6 +67,34 @@ router.delete('/delete/:id', async(req, res) => {
         console.log(error);
         res.status(500).json(error)
     }
+});
+
+//subscription usage
+
+router.post('/:id/usage_records', async(req, res) => {
+    try {
+        const usageRecord = await stripe.subscriptionItems.createUsageRecord(
+            req.params.id, { quantity: req.body.quantity, timestamp: req.params.timestamp }
+        );
+        res.json(usageRecord);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+
+    }
+});
+
+router.get('/usage_record/:id/summaries', async(req, res) => {
+    try {
+        const usageRecordSummaries = await stripe.subscriptionItems.listUsageRecordSummaries(
+            req.params.id, { limit: 10 }
+        );
+        res.send(usageRecordSummaries)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router;
