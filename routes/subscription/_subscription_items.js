2bbb5ba094c50c8cfac884fe47a51e9d296a6e17
fsmutimeer <ishpata@hotmail.com>
2022-01-12 23:10:11 -0800

    subscritionItems add

---
+++ routes/subscription/_subscription_items.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+require('dotenv/config')
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.SK);
+
+
+router.post('/create', async(req, res) => {
+    try {
+        const subscriptionItem = await stripe.subscriptionItems.create({
+            subscription: req.body.subscription,
+            price: req.body.price,
+            quantity: req.body.quantity,
+        });
+        res.json(subscriptionItem);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+
+    }
+});
+
+router.get('/view/:id', async(req, res) => {
+    try {
+        const subscriptionItem = await stripe.subscriptionItems.retrieve(
+            req.params.id
+        );
+        res.send(subscriptionItem)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+})
+
+router.get('/view', async(req, res) => {
+    try {
+        const subscriptionItems = await stripe.subscriptionItems.list({
+            subscription: req.body.subscription_id, //The ID of the subscription whose items will be retrieved.
+        });
+        res.send(subscriptionItems)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+});
+
+router.post('/update/:id', async(req, res) => {
+    try {
+        const subscriptionItem = await stripe.subscriptionItems.update(
+            req.params.id, { metadata: { order_id: req.body.order_id } }
+        );
+        res.json(subscriptionItem);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+
+    }
+});
+router.delete('/delete/:id', async(req, res) => {
+    try {
+        const deleted = await stripe.subscriptionItems.del(
+            req.params.id
+        );
+        res.send(deleted)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router;
